2ce3b2b451cfffc6ed5fffa14ac23b00a4684610
Anton <44608928+aneroid13@users.noreply.github.com>
2020-02-27 12:16:59 +0700

    Update vmware_guest_rdm.py

--- lib/ansible/modules/cloud/vmware/vmware_guest_rdm.py
+++ lib/ansible/modules/cloud/vmware/vmware_guest_rdm.py
@@ -6,12 +6,6 @@
 
 from __future__ import absolute_import, division, print_function
 
-ANSIBLE_METADATA = {
-    'metadata_version': '0.1',
-    'status': ['preview'],
-    'supported_by': 'community'
-}
-
 DOCUMENTATION = r'''
 ---
 module: vmware_guest_rdm
